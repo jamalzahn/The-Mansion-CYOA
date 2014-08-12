64e1117bd094125a850c5af450cd1f3ccce62c55
kimuri42 <baritone1315@gmail.com>
2014-08-12 14:12:44 -0500

    player and page classes
    starting framework

---
+++ src/Player.java
@@ -0,0 +1,8 @@
+/*Player will store all items, count items and the last page visited
+*This will also handle the final score, may need to store hardcore vs normal mode
+*need to have a way to add items and check if they are there or not
+*/
+public class Player 
+{
+	
+}
